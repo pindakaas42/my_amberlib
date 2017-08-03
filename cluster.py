fc4525c6347e699e037ff92c07564b69139bf971
Paul Westphaelinger <paul_westphaelinger@ewetel.net>
2017-08-03 14:21:24 +0200

    fixes to clusterlib the new artificial colvar function

--- cluster.py
+++ cluster.py
@@ -105,25 +105,27 @@ def cluster_epsilon(trajectory, topology, targetdir, epsilon='3.0',
     os.system('mpirun -np 4 cpptraj.MPI -i '+cpptrajfile)
     os.chdir(nowdir)
 
+
 def arti_collv(trajs, topology):
     top = pt.load_topology(topology)
     traj = pt.iterload(trajs, top)
     atoms = lib.find_p_atoms_pytraj(topology)
     mask = '@'+','.join(atoms[0])+' @'+','.join(atoms[1])
     dist = pt.distance(traj, top=top, mask=mask)
-    bins= np.linspace(min(dist), max(dist), 30)
+    bins = np.linspace(min(dist), max(dist), 30)
     binned = np.digitize(dist, bins)
     trajs_indxs = dict()
-    trajs_indxs = {i:None for i in binned}
+    trajs_indxs = {i: None for i in binned}
     for k, val in enumerate(binned):
         if trajs_indxs[val] is None:
             trajs_indxs[val] = [k]
         else:
             trajs_indxs[val].append(k)
-    return trajs_indxs
+    return trajs_indxs, bins
+
 
 def write_arti_collv(trajs, topology):
-    trajs_indxs = arti_collv(trajs, topology)
+    trajs_indxs, bins = arti_collv(trajs, topology)
     traj = pt.iterload(trajs, topology)
     for key, val in trajs_indxs.iteritems():
         pt.write_traj('../art_cluster/window'+str(key)+'.nc',
